a29726fe2b89cbbd99aeef12ba780498c75f6f5e
Haroen Vermylen <haroen@cumul.io>
2017-07-06 12:05:15 +0200

    Updated embedding example to use responsive resizer

--- example-embedding.js
+++ example-embedding.js
@@ -11,11 +11,9 @@ var client = new Cumulio({
 var server = http.createServer(function(request, response) {
 
   // Create a temporary token
-  var dashboard = '1d5db81a-3f88-4c17-bb4c-d796b2093dac';
+  var dashboardId = '6ae6a28d-b074-494f-a9bb-6b6ee7432973';
   client.create('authorization', {
     type: 'temporary',
-    // Exhaustive list of datasets and dashboards to which to grant access
-    securables: ['4db23218-1bd5-44f9-bd2a-7e6051d69166', 'f335be80-b571-40a1-9eff-f642a135b826', dashboard],
     // List of data filters to apply, so clients can only access their own data
     filters: [
       {
@@ -29,32 +27,40 @@ var server = http.createServer(function(request, response) {
     ],
     // Expire the token after 5 minutes
     expiry: new Date(new Date().getTime() + 300*1000),
-    // Force the screen mode to desktop (regardless of the iframe size) & language to English
-    screenmode: 'desktop',
-    locale_id: 'en'
   })
     .then(function(authorization) {
-      // Generate the embedding url
-      return client.iframe(dashboard, authorization);
-    })
-    .then(function(iframe) {
       // In reality, you might want to use a templating engine for this :-)
-      response.end(
-        '<!DOCTYPE html>' +
-        '<html>' +
-        '  <head>' +
-        '    <meta charset="UTF-8">' +
-        '    <title>Cumul.io embedding example</title>' +
-        '  </head>' +
-        '  <body>' +
-        '    <div style="margin-left: 28px; width: 650px;">' +
-        '      <h1 style="font-weight: 200;">Cumul.io embedding example</h1>' +
-        '      <p>This page contains an example of an embedded dashboard of Cumul.io. The dashboard data is securely filtered server-side, so clients can only access data to which your application explicitly grants access (in this case, the "Damflex" product).</p>' +
-        '    </div>' +
-        '    <iframe src="' + iframe + '" style="border: 0; width: 1024px; height: 650px;"></iframe>' +
-        '  </body>' +
-        '</html>'
-      );
+      response.end(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="UTF-8">
+            <title>Cumul.io embedding example</title>
+          </head>
+          <body style="margin-left: 100px;">
+            <div style="width: 650px;">
+              <h1 style="font-weight: 200;">Cumul.io embedding example</h1>
+              <p>This page contains an example of an embedded dashboard of Cumul.io. The dashboard data is securely filtered server-side, so clients can only access data to which your application explicitly grants access (in this case, the "Damflex" product).</p>
+              <p>Try to resize your page to see the dashboard adapting to different screen modes.</p>
+            </div>
+
+            <div id="myDashboard"></div>
+            <script type="text/javascript">
+              (function(d, a, s, h, b, oa, rd) { 
+                if (!d[b]) {oa = a.createElement(s), oa.async = 1; oa.src = h; rd = a.getElementsByTagName(s)[0]; rd.parentNode.insertBefore(oa, rd);}
+                d[b] = d[b] || {}; d[b].addDashboard = d[b].addDashboard || function(v) { (d[b].list = d[b].list || []).push(v) };
+              })(window, document, 'script', 'https://cdn-a.cumul.io/js/embed.min.js', 'Cumulio');
+              Cumulio.addDashboard({
+                dashboardId: '${dashboardId}'
+                , container: '#myDashboard'
+                , key: '${authorization.id}'
+                , token: '${authorization.token}'
+              });
+            </script>
+
+          </body>
+        </html>
+      `);
     })
     .catch(function(error) {
       console.error(error);
