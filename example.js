79bddf2dc824265f94b5543aa4982ed8da29a3f2
Tuana Celik <tuana@cumul.io>
2021-03-12 16:01:50 +0000

    example ts file and complete types, tsdoc start

--- example.js
+++ example.js
@@ -1,81 +1,84 @@
-var Cumulio = require('./cumulio');
+var Cumulio = require("./src/cumulio");
 
 // Connect
 var client = new Cumulio({
-  api_key: '< Your API key >',
-  api_token: '< Your API token >'
+  api_key: "< Your API key >",
+  api_token: "< Your API token >",
 });
 
 // Example 1: create a new dataset
 var dataset;
 
-client.create('securable', {
-  type: 'dataset',
-  name: {
-    nl: 'Burrito-statistieken',
-    en: 'Burrito statistics'
-  }
-})
+client
+  .create("securable", {
+    type: "dataset",
+    name: {
+      nl: "Burrito-statistieken",
+      en: "Burrito statistics",
+    },
+  })
 
   // Example 2: update a dataset
-  .then(function(result) {
+  .then(function (result) {
     dataset = result;
-    return client.update('securable', dataset.id, {description: {nl: 'Het aantal geconsumeerde burrito\'s per type'}});
+    return client.update("securable", dataset.id, {
+      description: { nl: "Het aantal geconsumeerde burrito's per type" },
+    });
   })
 
   // Example 3: create columns
-  .then(function(dataset) {
+  .then(function (dataset) {
     return client.create(
-      'column', {
-        type: 'hierarchy',
-        format: '',
-        informat: 'hierarchy',
+      "column",
+      {
+        type: "hierarchy",
+        format: "",
+        informat: "hierarchy",
         order: 0,
-        name: {nl: 'Type burrito'}
+        name: { nl: "Type burrito" },
       },
       [
         {
-          role: 'Securable',
-          id: dataset.id
-        }
+          role: "Securable",
+          id: dataset.id,
+        },
       ]
     );
   })
-  .then(function() {
+  .then(function () {
     return client.create(
-      'column', {
-        type: 'numeric',
-        format: ',.0f',
-        informat: 'numeric',
+      "column",
+      {
+        type: "numeric",
+        format: ",.0f",
+        informat: "numeric",
         order: 1,
-        name: {nl: 'Burrito-gewicht'}
+        name: { nl: "Burrito-gewicht" },
       },
       [
         {
-          role: 'Securable',
-          id: dataset.id
-        }
+          role: "Securable",
+          id: dataset.id,
+        },
       ]
     );
   })
 
   // Example 4: push 2 data points to a dataset
-  .then(function() {
-    return client.create(
-      'data',
-      {
-        securable_id: dataset.id,
-        data: [
-          ['sweet', 126],
-          ['sour',  352]
-        ]
-      });
+  .then(function () {
+    return client.create("data", {
+      securable_id: dataset.id,
+      data: [
+        ["sweet", 126],
+        ["sour", 352],
+      ],
+    });
   })
 
   // Error handling & closing connection
-  .catch(function(error) {
-    console.error('API error:', error);
+  .catch(function (error) {
+    console.error("API error:", error);
   })
-  .finally(function() {
+  .finally(function () {
     client.close();
   });
---
+++ example_ts.ts
@@ -0,0 +1,4 @@
+import Cumulio from "cumulio";
+
+const options: Cumulio.Options = { api_key: "", api_token: "" };
+const client = new Cumulio(options);
